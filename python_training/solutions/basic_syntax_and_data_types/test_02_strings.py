c43c72e06186d6a700068b478c310329bdfb2f15
lucas-nelson-uiuc <lucas.nelson.uiuc@gmail.com>
2024-07-20 12:09:51 -0500

    build: solutions link with topics

--- python_training/solutions/basic_syntax_and_data_types/test_02_strings.py
+++ python_training/solutions/basic_syntax_and_data_types/test_02_strings.py
@@ -1,18 +1,39 @@
 import pytest
 from python_training.topics.basic_syntax_and_data_types.exercises.topic_02_strings import (
     SUBMIT_SOLUTION,
-    # ... other tests here ...
+    string_concatenation,
+    string_escaping,
+    string_formatting,
+    string_repetition
 )
 
 
-# @pytest.mark.parametrize()
 @pytest.mark.skipif(not SUBMIT_SOLUTION, reason="Solution not yet submitted")
 class TestUserInput:
-    def test_first_case(self):
-        assert True
+    def test_string_repetition(self):
+        return_value = string_repetition().get("string_rep")
+        assert isinstance(return_value, str)
+        assert return_value == "randomrandomrandom"
 
-    def test_second_case(self):
-        assert True
+    def test_string_concatenation(self):
+        return_value = string_concatenation()
+        return_keys = ("first_name", "last_name", "full_name")
+        first_name, last_name, full_name = (
+            return_value.get(var) for var in return_keys
+        )
+        assert all(isinstance(var, str) for var in (first_name, last_name, full_name))
+        assert full_name == first_name + " " + last_name
 
-    def test_third_case(self):
-        assert True
+    def test_string_formatting(self):
+        return_value = string_formatting()
+        return_keys = ("first_name", "last_name", "full_name")
+        first_name, last_name, full_name = (
+            return_value.get(var) for var in return_keys
+        )
+        assert all(isinstance(var, str) for var in (first_name, last_name, full_name))
+        assert full_name == f"{first_name} {last_name}"
+
+    def test_string_escaping(self):
+        return_value = string_escaping()
+        assert return_value.get("newline_char") == "\n"
+        assert return_value.get("tab_char") == "\t"
--- python_training/topics/basic_syntax_and_data_types/exercises/topic_02_strings.py
+++ python_training/topics/basic_syntax_and_data_types/exercises/topic_02_strings.py
@@ -1,10 +1,36 @@
-### {{ exercise_name }}
-# {{ exercise_description }}
+### {{ topic_name }}
+# {{ topic_description }}
 
 
-SUBMIT_SOLUTION = False
+SUBMIT_SOLUTION = True
 
 
-def user_input():
-    ### TODO: ...
-    pass
+def string_repetition():
+    string_var = "random"
+    # TODO: assign `string_rep` as the value of `string_var` repeated 3 times
+    string_rep = ...
+    return locals()
+
+
+def string_concatenation():
+    first_name = "First"
+    last_name = "Last"
+    # TODO: assign the variable `full_name` as "First Last" using string concatenation
+    full_name = ...
+    return locals()
+
+
+def string_formatting():
+    first_name = "First"
+    last_name = "Last"
+    # TODO: assign the variable `full_name` as "First Last" using f-string formatting
+    full_name = ...
+    return locals()
+
+
+def string_escaping():
+    # TODO: assign the variable `newline_character` as the newline escape sequence
+    newline_char = ...
+    # TODO: assign the variable `tab_character` as the tab escape sequence
+    tab_char = ...
+    return locals()
