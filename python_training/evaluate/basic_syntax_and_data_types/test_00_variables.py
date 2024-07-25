f6a14cbd3f2eca0a0089a12e1c3767773b536d9a
lucas-nelson-uiuc <lucas.nelson.uiuc@gmail.com>
2024-07-24 21:56:49 -0500

    build: completed variables section

--- python_training/evaluate/basic_syntax_and_data_types/test_00_variables.py
+++ python_training/evaluate/basic_syntax_and_data_types/test_00_variables.py
@@ -1,18 +1,31 @@
 import pytest
 from python_training.topics.basic_syntax_and_data_types.exercises.topic_00_variables import (
     SUBMIT_SOLUTION,
-    # ... other tests here ...
+    assign_variables,
+    fix_data_types,
+    cast_data_types
 )
 
 
-# @pytest.mark.parametrize()
 @pytest.mark.skipif(not SUBMIT_SOLUTION, reason="Solution not yet submitted")
 class TestUserInput:
-    def test_first_case(self):
-        assert True
+    def test_assign_variables(self):
+        test_scope = assign_variables()
+        assert isinstance(test_scope.get("string_var"), str)
+        assert isinstance(test_scope.get("integer_var"), int)
+        assert isinstance(test_scope.get("float_var"), float)
+        assert isinstance(test_scope.get("boolean_var"), bool)
 
-    def test_second_case(self):
-        assert True
+    def test_fix_data_types(self):
+        test_scope = fix_data_types()
+        assert isinstance(test_scope.get("string_var"), str)
+        assert isinstance(test_scope.get("integer_var"), int)
+        assert isinstance(test_scope.get("float_var"), float)
+        assert isinstance(test_scope.get("boolean_var"), bool)
 
-    def test_third_case(self):
-        assert True
+    def test_cast_data_types(self):
+        test_scope = fix_data_types()
+        assert isinstance(test_scope.get("string_var"), str)
+        assert isinstance(test_scope.get("integer_var"), int)
+        assert isinstance(test_scope.get("float_var"), float)
+        assert isinstance(test_scope.get("boolean_var"), bool)
--- python_training/solutions/basic_syntax_and_data_types/topic_00_variables.py
+++ python_training/solutions/basic_syntax_and_data_types/topic_00_variables.py
@@ -1,10 +1,52 @@
-### {{ exercise_name }}
-# {{ exercise_description }}
+### Variable Naming and Assignment
 
 
 SUBMIT_SOLUTION = False
 
 
-def user_input():
-    ### TODO: ...
-    pass
+def assign_variables():
+    # TODO: assign a string value to the variable `string_var`
+    string_var = "randomstring"
+
+    # TODO: assign an integer value to the variable `integer_var`
+    integer_var = 12
+
+    # TODO: assign a float value to the variable `float_var`
+    float_var = 12.34
+
+    # TODO: assign a boolean value to the variable `boolean_var`
+    boolean_var = True
+
+    return locals()
+
+
+def fix_data_types():
+    # TODO: change the code below to assign `string_var` a string value
+    string_var = "680.55"
+
+    # TODO: change the code below to assign `integer_var` an integer value
+    integer_var = 12
+
+    # TODO: change the code below to assign `float_var` a float value
+    float_var = 12.34
+
+    # TODO: change the code below to assign `boolean_var` a boolean value
+    boolean_var = True
+
+    return locals()
+
+
+def cast_data_types():
+    # TODO: convert the value into a string type using the str() function
+    string_var = str(12)
+
+    # TODO: convert the value into an integer type using the int() function
+    integer_var = int(12.34)
+
+    # TODO: convert the value into a float type using the float() function
+    float_var = float(12)
+
+    # TODO: convert the value into a boolean type using the bool() function
+    boolean_var = bool(0)
+
+    return locals()
